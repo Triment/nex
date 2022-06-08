968cfb7d9cdc5f6e46f8731279b64775b3ab2e66
triment <lsplub@gmail.com>
2022-06-08 11:23:33 +0800

    Initial Commit

---
+++ nex/nexApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexApp.swift
+//  nex
+//
+//  Created by triment on 2022/6/8.
+//
+
+import SwiftUI
+
+@main
+struct nexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
